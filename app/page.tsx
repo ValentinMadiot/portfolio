f8de4b338986dbb402251c6644e69da6d47f2ba9
valentinmadiot <valentin.madiot@gmail.com>
2025-01-08 20:27:27 +0700

    home-page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
@@ -5,7 +6,7 @@ export default function Home() {
     <div className="">
       <main className="relative bg-black-100 flex flex-col items-center justify-center overflow-hidden mx-auto sm:px-10 px-5">
         <div className="max-w-7xl w-full">
-          <h1>Portfolio</h1>
+          <Hero />
         </div>
       </main>
       <footer></footer>
