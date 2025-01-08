4805d759bc4289a5ddf5ade1dd1b5411126e82fa
valentinmadiot <valentin.madiot@gmail.com>
2025-01-08 16:21:56 +0700

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <main className="relative bg-black-100 flex flex-col items-center justify-center overflow-hidden mx-auto sm:px-10 px-5">
+        <div className="max-w-7xl w-full">
+          <h1>Portfolio</h1>
+        </div>
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
