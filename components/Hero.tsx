2cea17ba0942379d1d324dd7b7057190c5ac6d57
valentinmadiot <valentin.madiot@gmail.com>
2025-01-25 02:11:13 +0700

    change hero background

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,6 +4,7 @@ import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect';
 import { FaLocationArrow } from 'react-icons/fa';
 import { socialMedia } from '@/data';
+import { DotBackgroundDemo } from './ui/DotBackground';
 
 const Hero = () => {
   return (
@@ -19,10 +20,8 @@ const Hero = () => {
         />
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
-      <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.025] bg-grid-black/[0.2] absolute top-0 left-0 flex items-center justify-center">
-        {/* Radial gradient for the container to give a faded look */}
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
-      </div>
+        <DotBackgroundDemo />
+
       <div className="flex justify-center relative z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <h2 className="uppercase tracking-widest text-xl text-center max-w-80">
